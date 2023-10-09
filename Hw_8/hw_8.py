626934760d4f0f6f83053741ffa2e963b54c5876
nkho29 <nazar17ho@gmail.com>
2023-10-09 15:55:47 +0300

    Commit3

--- Hw_8/hw_8.py
+++ Hw_8/hw_8.py
@@ -46,5 +46,24 @@ play = "games"
 my_hobby = "racing"
 eat = "food"
 
-print(f"Hello!, My name is {my_name}\n, i born in {birth_year} in {my_country}\n, {my_pet} a pet,\n my fav book is {my_book},\n i like to play {play}\n, my hobby is {my_hobby}, i eat {eat}")
+print(f"Hello!, My name is {my_name}\ni born in {birth_year} in {my_country}\n{my_pet} a pet\nmy fav book is {my_book}\ni like to play {play}\nmy hobby is {my_hobby}, i eat {eat}")
 
+alphabet1 = "a b c d e f g h i g k l m n o p q r s t u v w x y z" #50
+
+print("-" * 100)
+
+Freedom = alphabet1[10:11:] + alphabet1[34:35:] + alphabet1[8:9:] + alphabet1[8:9:] + alphabet1[6:7] + alphabet1[28:29] +alphabet1[24:25]
+
+print(f"{Freedom}")
+
+print("-" * 100)
+
+hope = alphabet1[14:15] + alphabet1[28:29] + alphabet1[30:31] + alphabet1[8:9]
+that = alphabet1[38:39] + alphabet1[14:15] + alphabet1[0:1] + alphabet1[38:39]
+you = alphabet1[48:49] + alphabet1[28:29] + alphabet1[40:41]
+used = alphabet1[40:41] + alphabet1[36:37] + alphabet1[8:9] + alphabet1[6:7]
+HTML = alphabet1[14:15] + alphabet1[38:39] + alphabet1[24:25] + alphabet1[22:23]
+
+print(f"{alphabet1[16:17].upper()} {hope} {that} {you} {used} {HTML.upper()}!")
+
+print("-" * 100)
