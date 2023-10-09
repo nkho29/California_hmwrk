cdddd7d975031955e2d2d2238194f1191cffbdb3
nkho29 <nazar17ho@gmail.com>
2023-10-09 15:01:31 +0300

    Commit1

---
+++ Hw_8/hw_8.py
@@ -0,0 +1,38 @@
+print("-" * 100)
+
+string1 = "44"
+
+print(type(string1))
+print(id(string1))
+
+print("-" * 100)
+
+integer1 = 45
+print(type(integer1))
+print(id(integer1))
+
+print("-" * 100)
+
+sum_of_all = int(string1) - integer1
+print(sum_of_all)
+
+print("-" * 100)
+
+num_1 = 36
+print(str(num_1))
+num_2 = 168
+print(str(num_2))
+num_3 = 25.65
+print(str(num_3))
+
+print("-" * 100)
+
+str_1 = "36"
+print(int(str_1))
+str_2 = "168"
+print(int(str_2))
+str_3 = "25.65"
+print(float(str_3))
+
+print("-" * 100)
+
